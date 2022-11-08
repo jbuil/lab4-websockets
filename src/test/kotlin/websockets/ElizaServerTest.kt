1ed5bd2cc754033de48a9a004fcd6dc209e19337
Jorge Buil <797285@unizar.es>
2022-11-08 18:35:34 +0100

    Detekt fix

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -1,9 +1,8 @@
+@file:Suppress("NoWildcardImports")
 package websockets
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*
@@ -67,7 +66,7 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
         list.add(message)
         latch.countDown()
         if (list.size == 3) {
-            session.basicRemote.sendText("always");
+            session.basicRemote.sendText("always")
         }
     }
 }
